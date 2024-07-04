90de7a4613ebd518c63b5ed3ac549916ed23c5e2
Swathi Jettiboina <110420243+Swathijetti2004@users.noreply.github.com>
2024-07-04 21:33:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum Number in a sorted rotated array/minimum-number-in-a-sorted-rotated-array.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Array
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine());
+            String st[] = read.readLine().trim().split("\\s+");
+            
+            int arr[] = new int[n];
+            
+            for(int i = 0; i < n; i++)
+              arr[i] = Integer.parseInt(st[i]);
+              
+            System.out.println(new Solution().minNumber(arr,0,n-1));
+        }
+    }
+    
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the minimum element in sorted and rotated array.
+    static int minNumber(int nums[], int l, int r)
+    {
+        // Your code here
+        l=0;
+        r=nums.length-1;
+        int mid;
+        while(l<r){
+            mid=(l+r)/2;
+            if(nums[mid]>nums[r]){
+                l=mid+1;
+            }else{
+                r=mid;
+            }
+        }
+        return nums[l];
+    }
+}
