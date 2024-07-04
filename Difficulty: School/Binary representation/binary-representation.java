55c7c49f07c9c1acf92f68f174a0d96d3056670c
Swathi Jettiboina <110420243+Swathijetti2004@users.noreply.github.com>
2024-07-04 14:13:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Binary representation/binary-representation.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N = sc.nextInt();
+
+            Solution ob = new Solution();
+            String ans = ob.getBinaryRep(N);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static String getBinaryRep(int N){
+        // code here
+        String s="";
+        while(N>0){
+            s=Integer.toString(N%2)+s;
+            N/=2;
+        }
+        int l=30-s.length();
+        
+        return "0".repeat(l)+s;
+    }
+}
