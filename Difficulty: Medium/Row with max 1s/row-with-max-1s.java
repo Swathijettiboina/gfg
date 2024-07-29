936c5852c1d0c63361b8fd630ca425bb7e84e27b
Swathi Jettiboina <110420243+Swathijetti2004@users.noreply.github.com>
2024-07-30 00:35:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int arr[][] = new int[n][m];
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+            int ans = new Solution().rowWithMax1s(arr);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int rowWithMax1s(int arr[][]) {
+        // code here
+        int col=0;
+        int m=arr[0].length;
+        while(col<m){
+            for(int i=0;i<arr.length;i++){
+                if(arr[i][col]==1){
+                    return i;
+                }     
+            }
+            col++;
+        }
+        
+        return -1;
+    }
+}
